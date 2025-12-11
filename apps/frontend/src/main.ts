4f3dd5976e4f7f2e080e4b1689caf30b5540a8a3
tone <tonesc@qq.com>
2025-12-11 12:35:53 +0800

    feat: 前端引入primeicons

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
 import './style.css'
+import 'primeicons/primeicons.css'
 import App from './App.vue'
 import { router } from './router';
 
